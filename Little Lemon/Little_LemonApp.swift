317fdc413a8b30f2082e0505f36f4a8f8e9ea89f
hasan-hayder <hasan.heidar2001@gmail.com>
2023-02-14 15:48:39 +0400

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Hasan Hayder on 2023-02-14.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
